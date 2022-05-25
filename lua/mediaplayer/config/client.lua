c0b262b092dbe14d1d2f4cdba57b86ddb6ad854e
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-05-25 20:18:39 +0200

    Update URL paths

--- lua/mediaplayer/config/client.lua
+++ lua/mediaplayer/config/client.lua
@@ -12,7 +12,7 @@ MediaPlayer.SetConfig({
 		-- Base URL where HTML content is located.
 		-- @type String
 		--
-		base_url = "http://samuelmaddock.github.io/gm-mediaplayer/"
+		base_url = "https://gmod-mediaplayer.pages.dev/"
 
 	},
 
@@ -25,7 +25,7 @@ MediaPlayer.SetConfig({
 		-- URL of the request menu.
 		-- @type String
 		--
-		url = "http://samuelmaddock.github.io/gm-mediaplayer/request.html"
+		url = "https://gmod-mediaplayer.pages.dev/request.html"
 
 	}
 
