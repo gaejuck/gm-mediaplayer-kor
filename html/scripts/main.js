4d1a391c2cb759161b095999c1620c7ecc1a264b
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-05-27 11:45:18 +0200

    Delete main.js

--- html/scripts/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-window.MP = function () {
-    var a = document.body;
-    return {
-        setHtml: function (b) {
-            a.innerHTML = b, console.log(a.innerHTML)
-        }
-    }
-}();
