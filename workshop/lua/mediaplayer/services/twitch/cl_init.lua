b61d16f91ecdf5a01758b45df080a9d91ec6b715
Shadowsun™ <12494967+CattoGamer@users.noreply.github.com>
2024-08-05 20:27:19 +0200

    Update Twitch

--- workshop/lua/mediaplayer/services/twitch/cl_init.lua
+++ workshop/lua/mediaplayer/services/twitch/cl_init.lua
@@ -2,75 +2,79 @@ include "shared.lua"
 
 DEFINE_BASECLASS( "mp_service_browser" )
 
+local JS_Pause = "if(window.MediaPlayer) MediaPlayer.pause();"
+local JS_Play = "if(window.MediaPlayer) MediaPlayer.play();"
+local JS_Volume = "if(window.MediaPlayer) MediaPlayer.volume = %s;"
+local JS_Seek = [[
+	if (window.MediaPlayer) {
+		var seekTime = %s;
+		var curTime = window.MediaPlayer.currentTime;
+
+		var diffTime = Math.abs(curTime - seekTime);
+		if (diffTime > 5) {
+			window.MediaPlayer.currentTime = seekTime
+		}
+	}
+]]
+
+local JS_Interface = [[
+	var checkerInterval = setInterval(function() {
+		var matureAccept = document.querySelectorAll("[data-a-target=\"content-classification-gate-overlay-start-watching-button\"]")[0]
+		if (!!matureAccept) {matureAccept.click(); return;}
+
+		var player = document.getElementsByTagName('video')[0];
+		var adOverlay = document.querySelectorAll("[data-test-selector=\"sad-overlay\"]")[0]
+
+		if (!adOverlay && !!player && player.paused == false && player.readyState == 4) {
+			clearInterval(checkerInterval);
+
+			window.MediaPlayer = player;
+		}
+	}, 50);
+]]
+
 function SERVICE:GetURL()
 	local channel = self:GetTwitchChannel()
 	return ("https://player.twitch.tv/?channel=%s&parent=pixeltailgames.com"):format( channel )
 end
 
-do -- Media Hook
-	local JS_Interface = [[
-		var checkerInterval = setInterval(function() {
-			var matureAccept = document.querySelectorAll("[data-a-target=\"player-overlay-mature-accept\"]")[0]
-			if (!!matureAccept) {matureAccept.click(); return;}
+function SERVICE:OnBrowserReady( browser )
 
-			var player = document.getElementsByTagName('video')[0];
-			var adOverlay = document.querySelectorAll("[data-test-selector=\"sad-overlay\"]")[0]
-
-			if (!adOverlay && !!player && player.paused == false && player.readyState == 4) {
-				clearInterval(checkerInterval);
-
-				window.MediaPlayer = player;
-			}
-		}, 50)
-	]]
-
-	function SERVICE:OnBrowserReady( browser )
-
-		BaseClass.OnBrowserReady( self, browser )
-
-		browser:OpenURL( self:GetURL() )
-		browser.OnDocumentReady = function(pnl)
-			browser:QueueJavascript( JS_Interface )
-		end
-
+	-- Resume paused player
+	if self._TTVPaused then
+		self.Browser:RunJavascript( JS_Play )
+		self._TTVPaused = nil
+		return
 	end
+
+	BaseClass.OnBrowserReady( self, browser )
+
+	browser:OpenURL( self:GetURL() )
+	browser.OnDocumentReady = function(pnl)
+		browser:QueueJavascript( JS_Interface )
+	end
+
 end
 
-do	-- Media Controls
-	local JS_Pause = "if(window.MediaPlayer) MediaPlayer.pause();"
-	local JS_Volume = "if(window.MediaPlayer) MediaPlayer.volume = %s;"
-	local JS_Seek = [[
-		if (window.MediaPlayer) {
-			var seekTime = %s;
-			var curTime = window.MediaPlayer.currentTime;
+function SERVICE:Pause()
+	BaseClass.Pause( self )
 
-			var diffTime = Math.abs(curTime - seekTime);
-			if (diffTime > 5) {
-				window.MediaPlayer.currentTime = seekTime
-			}
-		}
-	]]
-
-	function SERVICE:Pause()
-		BaseClass.Pause( self )
-
-		if IsValid(self.Browser) then
-			self.Browser:RunJavascript(JS_Pause)
-			self._YTPaused = true
-		end
-
+	if IsValid(self.Browser) then
+		self.Browser:RunJavascript(JS_Pause)
+		self._TTVPaused = true
 	end
 
-	function SERVICE:SetVolume( volume )
-		local js = JS_Volume:format( MediaPlayer.Volume() )
-		self.Browser:RunJavascript(js)
-	end
+end
 
-	function SERVICE:Sync()
+function SERVICE:SetVolume( volume )
+	local js = JS_Volume:format( MediaPlayer.Volume() )
+	self.Browser:RunJavascript(js)
+end
 
-		local seekTime = self:CurrentTime()
-		if self:IsTimed() and seekTime > 0 then
-			self.Browser:RunJavascript(JS_Seek:format(seekTime))
-		end
+function SERVICE:Sync()
+
+	local seekTime = self:CurrentTime()
+	if self:IsTimed() and seekTime > 0 then
+		self.Browser:RunJavascript(JS_Seek:format(seekTime))
 	end
 end
